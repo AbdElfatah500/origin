5cd1e30b093a9da40e4f5384b6b3d47948d7b884
Abd Elfatah <hasn558559@gmail.com>
2024-07-31 11:55:13 +0300

    add new files

---
+++ HelloSwift.cpp
@@ -0,0 +1,18 @@
+// This is a C++ file 
+#include <iostream>
+using namespace std ;
+class HelloJava{
+public:
+    void printHelloworld(){
+        cout<<"Hello world"<<endl;
+
+
+    }
+};
+
+int main(){
+
+
+
+return 0
+}
